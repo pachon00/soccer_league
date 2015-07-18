afd6f236469d02967b6d0c8caaa3a528f59e5269
root <root@butters>
2015-07-18 14:29:59 -0500

    add routes and templates

--- soccer_league/app/router.js
+++ soccer_league/app/router.js
@@ -6,6 +6,15 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-});
+  this.route('football', { path: "/" }, function() {
+    this.route('new-team');
+    this.route('new-league');
+    this.route('new-category');
+  });
+  this.route("results");
+  this.route("football/new-team");
+  this.route("football/new-league");
+  this.route("football/new-category");
+  });
 
 export default Router;
